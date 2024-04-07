47e8cf60d06d2cfa87fb56b2f5f180bd0d272fbb
Viktoriia Lushpak <lushpak.pn@ucu.edu.ua>
2024-04-07 20:18:21 +0300

    Move Node task completed

--- Linked_Lists_-_Move_Node/move_node.py
+++ Linked_Lists_-_Move_Node/move_node.py
@@ -1,16 +1,23 @@
 """Move Node"""
 
 class Node(object):
+    """Node class"""
     def __init__(self, data):
         self.data = data
         self.next = None
-    
+
 class Context(object):
+    """Context class"""
     def __init__(self, source, dest):
         self.source = source
         self.dest = dest
-    
+
 def move_node(source, dest):
-    # Your code goes here.
-    # Remember to return the context.
-    return Context(source, dest)
+    """Move the node and return """
+    if not source:
+        raise Exception
+    new_source = source.data
+    next = source.next
+    new_dest = Node(new_source)
+    new_dest.next = dest
+    return Context(next, new_dest)
