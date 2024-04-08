573380b8ea796cd16822daab6a187be03604aa29
Viktoriia Lushpak <lushpak.pn@ucu.edu.ua>
2024-04-08 12:13:12 +0300

    Parse_a_linked_list_from_a_string fixed + tests

--- Parse_a_linked_list_from_a_string/parse.py
+++ Parse_a_linked_list_from_a_string/parse.py
@@ -1,22 +1,23 @@
 """Parse a linked list from a string"""
 
 class Node:
-    def __init__(self, data, next=None): 
+    """Node class"""
+    def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
 def linked_list_from_string(s):
-    # if s == 'None':
-    #     return None
+    """Return the list of Nodes from string"""
+    if s == 'None':
+        return None
     nodes = s.split(' -> ')
-    
-    head = Node(nodes[0])
-    current = head
-    
-    for node in nodes[1:]:
-        current.next = Node(node)
-        current = current.next
-    
-    return head
+    nodes = nodes[:-1]
 
-print(linked_list_from_string("1 -> 2 -> 3 -> None"))
+    head = Node(int(nodes[0]))
+    current = head
+
+    for node in nodes[1:]:
+        current.next = Node(int(node))
+        current = current.next
+
+    return head
